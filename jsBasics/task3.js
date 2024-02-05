63d22627df546d98d09ee4729c1233ea6871a87d
Pon20995 <ozera123456789@gmail.com>
2024-02-05 20:32:56 +0000

    added_files_with_homework_3

---
+++ jsBasics/task3.js
@@ -0,0 +1,18 @@
+let agePetro = 16;
+let ageStacy = 18;
+let ageVlad = 45;
+
+let adultAge = 18;
+
+let adultVerification1 = agePetro >= adultAge;
+let adultVerification2 = ageStacy >= adultAge; 
+let adultVerification3 = ageVlad >= adultAge;
+
+console.log('Petro is ' + agePetro + ' years old');
+console.log('The person is adult: ' + adultVerification1);
+console.log('---------------------------------------------');
+console.log('Stacy is ' + ageStacy + ' years old');
+console.log('The person is adult: ' + adultVerification2);
+console.log('---------------------------------------------');
+console.log('Vlad is ' + ageVlad + ' years old');
+console.log('The person is adult: ' + adultVerification3);
