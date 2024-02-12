c2ee60d93132ab629f9e0613ad3f828b1c541fc8
Pon20995 <ozera123456789@gmail.com>
2024-02-12 21:44:56 +0000

    added_files_with_homework_5

---
+++ jsLesson3/additional-task1.js
@@ -0,0 +1,19 @@
+import readline from "readline";
+
+const input = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function isEven(number) {
+  if (number % 2 === 0) {
+    return `The ${number} is even`;
+  } else {
+    return `The ${number} is not even`;
+  }
+}
+
+input.question("Введіть число: ", (number) => {
+  console.log(isEven(number));
+  input.close();
+});
