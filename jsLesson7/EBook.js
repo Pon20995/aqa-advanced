988b8e0b13771c72cb61378655561294e12a4bf0
Pon20995 <ozera123456789@gmail.com>
2024-02-27 14:00:07 +0000

    added_files_with_homework_9

---
+++ jsLesson7/EBook.js
@@ -0,0 +1,46 @@
+import { Book } from "./Book.js";
+
+class EBook extends Book {
+  constructor(name, author, year, format) {
+    super(name, author, year);
+    this.format = format;
+  }
+  get format() {
+    return this._format;
+  }
+  set format(newFormat) {
+    if (typeof newFormat === "string") {
+      this._format = newFormat;
+    } else {
+      console.log("Format is not a string");
+    }
+  }
+
+  printInfo() {
+    super.printInfo();
+    console.log(`in ${this.format} format`);
+  }
+
+  static createNewEBook(book, format) {
+    if (typeof format === "string") {
+      return new EBook(book.name, book.author, book.year, format);
+    } else {
+      console.log("Format is not a string");
+    }
+  }
+}
+const firstBook = new Book("Fight club", "Chuck Palahniuk", 1996);
+const secondBook = new Book("A Game of Thrones", "George R. R. Martin", 1996);
+const thirdBook = new Book("The Empire of the Angels", "Bernard Werber", 2000);
+const eBook = new EBook("Philosopher's Stone", "J. K. Rowling", 1997, "PDF");
+eBook.printInfo();
+
+EBook.isOldest([firstBook, secondBook, thirdBook, eBook]);
+
+const book = new Book(
+  "Shadows of Forgotten Ancestors",
+  "Mykhailo Kotsyubynsky",
+  1912
+);
+const newBook = EBook.createNewEBook(book, "EPUB");
+newBook.printInfo();
